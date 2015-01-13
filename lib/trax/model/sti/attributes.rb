93f8b445b01e6721847ae2971315498c3edbd5ee
Jason Ayre <jasonayre@gmail.com>
2015-01-12 18:24:33 -0700

    validating sti_attributes breaks on uniqueness_of with sti_attribute, added validates_uniqueness_of! method for sti

--- lib/trax/model/sti/attributes.rb
+++ lib/trax/model/sti/attributes.rb
@@ -18,7 +18,7 @@ module Trax
           self._attribute_set_column = :attribute_set_id
           self._attribute_set_class_name = "#{name}AttributeSet"
 
-          self.belongs_to(self._attribute_set_relation_name, :class_name => self._attribute_set_class_name)
+          self.belongs_to(self._attribute_set_relation_name, :class_name => self._attribute_set_class_name, :validate => true)
           self.validates(self._attribute_set_relation_name, :presence => true)
 
           self.before_create(:attribute_set)
@@ -60,6 +60,31 @@ module Trax
 
               self.delegate(attribute_name, :to => :attribute_set)
               self.delegate("#{attribute_name}=", :to => :attribute_set) unless options.key?(:writer) && options[:writer] == false
+            end
+          end
+
+          def validates_uniqueness_of!(*args)
+            options = args.extract_options!
+
+            args.each do |arg|
+              validation_method_name = :"validate_#{arg.to_s}"
+
+              self.send(:define_method, :"validate_#{arg.to_s}") do |*_args|
+                where_scope_hash = {}
+                field_value = self.__send__(arg)
+                where_scope_hash[arg] = field_value
+
+                where_scope = self._attribute_set_class_name.constantize.where(where_scope_hash).all
+
+                options[:scope].each do |field|
+                  scoped_field_value = self.__send__(field)
+                  where_scope.merge(self.class.where({field => scoped_field_value}))
+                end if options.has_key?(:scope)
+
+                errors.add(arg, "has already been taken") if where_scope.limit(1).any?
+              end
+
+              self.validate(validation_method_name)
             end
           end
         end
