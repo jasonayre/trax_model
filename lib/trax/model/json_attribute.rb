82d26475156ba67e96cb1d9d479fa1618480889f
Jason Ayre <jasonayre@gmail.com>
2015-04-16 20:40:14 -0600

    json attributes now definable inline in model, with specific schema, validations independent of model. BOOYA

--- lib/trax/model/json_attribute.rb
+++ lib/trax/model/json_attribute.rb
@@ -4,26 +4,11 @@ module Trax
   module Model
     class JsonAttribute < ::Hashie::Dash
       include Hashie::Extensions::IgnoreUndeclared
+      include ActiveModel::Validations
 
-      def initialize(*args, **named_args)
-        super(*args)
+      def inspect
+        self.to_hash.inspect
       end
-
-      def self.property(name, *args)
-        super(name, *args)
-
-        # define_method("#{name}=") do |val|
-        #   assert_property_required! name, val
-        #   assert_property_exists! name
-        #
-        #   @owner.__send__(@attribute_name)[name] = val
-        #   @owner[@attribute_name] = self.to_hash
-        # end
-      end
-
-      private
-
-      attr_accessor :owner, :attribute_name
     end
   end
 end
