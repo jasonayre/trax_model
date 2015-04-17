84fbcf7e5a9ddbe843860eb171742afeda3758b2
Jason Ayre <jasonayre@gmail.com>
2015-04-16 20:48:10 -0600

    remove pry and experimental code

--- lib/trax/model/railtie.rb
+++ lib/trax/model/railtie.rb
@@ -1,17 +1,10 @@
 module Trax
   module Model
     class Railtie < ::Rails::Railtie
-      initializer 'trax_model.active_record' do
-        require_relative "../active_record_extensions"
-      end
-
       ::ActiveSupport.on_load(:active_record) do
-        binding.pry
         def self.inherited(subklass)
           subklass.include(::Trax::Model) if ::Trax::Model.config.auto_include
           super(subklass)
-
-
         end
       end
     end
