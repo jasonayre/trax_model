2d8f70b7a3907bb76683b607e6395daec9d1ab53
Jason Ayre <jasonayre@gmail.com>
2015-04-22 10:02:18 -0600

    fix raising of error in json attribute if no hash structure provided, add error instead

--- lib/trax/validators/json_attribute_validator.rb
+++ lib/trax/validators/json_attribute_validator.rb
@@ -1,10 +1,17 @@
+# validates each json property and bubbles up any errors
+# also throws a generic can not be blank error, in the event
+# that a hash is not provided
 class JsonAttributeValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     json_attribute = object.class.json_attribute_fields[attribute]
 
-    unless value.valid?
-      value.errors.messages.each_pair do |k,v|
-        object.errors["#{attribute}.#{k}"] << v
+    unless value.is_a?(json_attribute) && value.valid?
+      if value.is_a?(json_attribute)
+        value.errors.messages.each_pair do |k,v|
+          object.errors["#{attribute}.#{k}"] << v
+        end
+      else
+        object.errors[attribute] = "can not be blank"
       end
     end
   end
