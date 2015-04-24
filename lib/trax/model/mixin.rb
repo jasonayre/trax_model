c80567ab9985bd4a77e2af0768b182c2b03af476
Jason Ayre <jayre@peer60.com>
2015-04-24 16:58:47 -0600

    wonky way of overriding mixin registry key

--- lib/trax/model/mixin.rb
+++ lib/trax/model/mixin.rb
@@ -3,9 +3,15 @@ module Trax
     module Mixin
       extend ::ActiveSupport::Concern
 
+      def self.before_included(&block)
+        prepend Module.new do
+          instance_eval(&block)
+        end
+      end
+
       included do
         self.extend(::ActiveSupport::Concern)
-        
+
         ::Trax::Model.register_mixin(self)
       end
     end
