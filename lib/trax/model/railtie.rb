b9a5612ab48a489af52c6a41ffaff106d676cc77
Jason Ayre <jayre@peer60.com>
2015-04-24 17:00:04 -0600

    can now have specific set of mixins auto incldued

--- lib/trax/model/railtie.rb
+++ lib/trax/model/railtie.rb
@@ -4,7 +4,12 @@ module Trax
       ::ActiveSupport.on_load(:active_record) do
         def self.inherited(subklass)
           subklass.include(::Trax::Model) if ::Trax::Model.config.auto_include
+
           super(subklass)
+
+          ::Trax::Model.config.auto_include_mixins.each do |mixin|
+            subklass.mixin(mixin)
+          end if ::Trax::Model.config.auto_include_mixins.any?
         end
       end
     end
