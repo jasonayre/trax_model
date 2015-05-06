88b87df2e3cb99385f3143aec6a91888554aa6a4
Jason Ayre <jasonayre@gmail.com>
2015-05-06 16:52:36 -0600

    uuid array

---
+++ lib/trax/model/attributes/types/uuid_array.rb
@@ -0,0 +1,87 @@
+require 'hashie/extensions/ignore_undeclared'
+
+module Trax
+  module Model
+    module Attributes
+      module Types
+        class UuidArray < ::Trax::Model::Attributes::Type
+          class Value < ::Trax::Model::Attributes::Value
+            # class_attribute :element_class
+            def initialize(*args)
+              @array = ::Trax::Model::UUIDArray.new(*args)
+            end
+
+            def __getobj__
+              @array
+            end
+
+            def inspect
+              @array.to_a.flatten.inspect
+            end
+
+            private
+          end
+
+          class TypeCaster < ActiveRecord::Type::Value
+            include ::ActiveRecord::Type::Mutable
+
+            def initialize(*args, target_klass:)
+              super(*args)
+
+              @target_klass = target_klass
+            end
+
+            def type
+              :uuid_array
+            end
+
+            def type_cast_from_user(value)
+              value.is_a?(@target_klass) ? @target_klass : @target_klass.new(value) || @target_klass.new
+            end
+
+            def type_cast_from_database(value)
+              value.present? ? @target_klass.new(*value) : @target_klass.new(nil)
+            end
+
+            def type_cast_for_database(value)
+              if value.present?
+                value.to_json
+                # value.map{ |val| val.is_a?(Trax::Model::UUID) ? }
+              else
+                nil
+              end
+            end
+          end
+
+          module Mixin
+            def self.mixin_registry_key; :uuid_array_attributes end;
+
+            extend ::Trax::Model::Mixin
+            include ::Trax::Model::Attributes::Mixin
+
+            module ClassMethods
+              def uuid_array_attribute(attribute_name, **options, &block)
+                attributes_klass_name = "#{attribute_name}_attributes".classify
+                attributes_klass = const_set(attributes_klass_name, ::Class.new(::Trax::Model::Attributes[:uuid_array]::Value))
+                attributes_klass.instance_eval(&block) if block_given?
+
+                attributes_klass.element_class = options[:of] if options.has_key?(:of)
+
+                trax_attribute_fields[:uuid_array] ||= {}
+                trax_attribute_fields[:uuid_array][attribute_name] = attributes_klass
+
+                if options.has_key?(:default)
+                  self.default_value_for(attribute_name, options[:default])
+                else
+                  []
+                end
+
+                attribute(attribute_name, ::Trax::Model::Attributes[:uuid_array]::TypeCaster.new(target_klass: attributes_klass))
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/trax/model/uuid_array.rb
@@ -0,0 +1,59 @@
+module Trax
+  module Model
+    class UUIDArray
+      include Enumerable
+
+      attr_reader :uuids
+
+      def initialize(*uuids)
+        @uuids = uuids.map{ |uuid_string| uuid_string.try(&:uuid) }.compact
+      end
+
+      def each(&block)
+        yield @uuids.each(&block)
+      end
+
+      def <<(val)
+        reset_instance_variables(:record_types, :records_grouped_by_count, :records_grouped_by_uuid)
+        @uuids.push(::Trax::Model::UUID.new(val))
+      end
+
+      def records
+        @records ||= group_by_record_type.to_a.map{|pair|
+          pair[0].by_id(*pair[1])
+        }.flatten.compact
+      end
+
+      def records_grouped_by_uuid
+        @records_grouped_by_uuid ||= records.group_by(&:id)
+      end
+
+      def records_grouped_by_count
+        @records_grouped_by_count ||= begin
+          {}.tap do |hash|
+            @uuids.group_by_count.each_pair do |uuid, count|
+              record = records_grouped_by_uuid[uuid].first
+              hash[record] = count
+            end
+
+            hash
+          end
+        end
+      end
+
+      private
+
+      def group_by_count
+        @uuids.group_by(&:count)
+      end
+
+      def group_by_record_type
+        @uuids.group_by{ |uuid| uuid.record_type }
+      end
+
+      def record_types
+        @record_types ||= @uuids.map(&:record_type).flatten.compact.uniq
+      end
+    end
+  end
+end
