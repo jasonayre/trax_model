423cd1858476b54590c9bb06da1f0e8ba855d5b3
Jason Ayre <jasonayre@gmail.com>
2015-05-27 19:26:22 -0600

    working on fixing display of enum

--- lib/trax/model/struct.rb
+++ lib/trax/model/struct.rb
@@ -11,13 +11,16 @@ module Trax
       include ::Hashie::Extensions::Dash::PropertyTranslation
       include ::ActiveModel::Validations
 
-      class_attribute :property_types
+      class_attribute :property_types, :fields
 
       def self.inherited(subklass)
         super(subklass)
 
+        subklass.fields = {}.dup
+
         subklass.property_types = {}.dup.tap do |hash|
           hash[:structs] = []
+          hash[:enums] = []
           hash
         end
       end
@@ -30,6 +33,30 @@ module Trax
         property(name, *args, **options)
         coerce_key(name, struct_klass)
         property_types[:structs].push(name)
+      end
+
+      def self.enum_property(name, *args, **options, &block)
+        enum_klass_name = "#{name}_enum".classify
+        enum_klass = const_set(enum_klass_name, ::Class.new(::Enum, &block))
+        options[:default] = {} unless options.key?(:default)
+        property(name, *args, **options)
+        coerce_key(name, enum_klass)
+        property_types[:enums].push(name)
+      end
+
+      class << self
+        alias :enum :enum_property
+        alias :struct :struct_property
+      end
+
+      class Definition < Hashie::Mash
+        def schema
+          {
+            :name => name,
+            :type => type,
+            :klass => klass
+          }
+        end
       end
     end
   end
