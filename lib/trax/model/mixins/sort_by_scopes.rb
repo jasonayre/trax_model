45f26b31e63ce976704a18f395b13c38585be424
Jason Ayre <jayre@peer60.com>
2015-06-22 12:49:22 -0600

    order scope sort direction should be flipped

--- lib/trax/model/mixins/sort_by_scopes.rb
+++ lib/trax/model/mixins/sort_by_scopes.rb
@@ -6,10 +6,10 @@ module Trax
 
         included do
           scope :sort_by_most_recent, lambda{|field_name='created_at'|
-            order("#{field_name} ASC")
+            order("#{field_name} DESC")
           }
           scope :sort_by_least_recent, lambda{|field_name='created_at'|
-            order("#{field_name} DESC")
+            order("#{field_name} ASC")
           }
 
           class << self
