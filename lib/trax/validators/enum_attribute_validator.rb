47fc4fb78bc70c7d6225bfe793163cf8409d5f63
Jason Ayre <jayre@peer60.com>
2015-09-02 16:52:35 -0600

    remove pry

--- lib/trax/validators/enum_attribute_validator.rb
+++ lib/trax/validators/enum_attribute_validator.rb
@@ -3,7 +3,6 @@ class EnumAttributeValidator < ActiveModel::EachValidator
     enum_attribute = object.class.fields_module[attribute]
 
     unless value.is_a?(enum_attribute) && enum_attribute === value
-      binding.pry
       object.errors[attribute] = "#{value} is not an allowed value"
     end
   end
