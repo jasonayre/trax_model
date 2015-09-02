f6838fb4825b133fba40a5545c1ebc9b454afdd8
Jason Ayre <jayre@peer60.com>
2015-09-02 13:54:56 -0600

    allow nil for enum

--- lib/trax/validators/enum_attribute_validator.rb
+++ lib/trax/validators/enum_attribute_validator.rb
@@ -1,8 +1,9 @@
 class EnumAttributeValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     enum_attribute = object.class.fields_module[attribute]
-    
+
     unless value.is_a?(enum_attribute) && enum_attribute === value
+      binding.pry
       object.errors[attribute] = "#{value} is not an allowed value"
     end
   end
