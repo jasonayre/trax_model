5359a11b8336bd42ce22d1ab72f55f56275af6f3
Jason Ayre <jasonayre@gmail.com>
2016-01-01 14:06:04 -0700

    relation methods for string attribute

--- lib/trax/model/extensions_for/string.rb
+++ lib/trax/model/extensions_for/string.rb
@@ -11,6 +11,18 @@ module Trax
             _values.flat_compact_uniq!
             model_class.where({field_name => _values})
           end
+
+          def eq_lower(*_values)
+            _values.flat_compact_uniq!
+            _values.map!(&:downcase)
+            model_class.where("lower(#{field_name}) IN(?)", _values)
+          end
+
+          def matches(*_values)
+            _values.flat_compact_uniq!
+            _values.map!(&:to_matchable)
+            model_class.where("(#{field_name}) ilike ANY(array[?])", _values)
+          end
         end
       end
     end
--- spec/trax/model/extensions_for/string_spec.rb
+++ spec/trax/model/extensions_for/string_spec.rb
@@ -12,6 +12,14 @@ describe ::Trax::Model::ExtensionsFor::String do
 
   context ".eq" do
     it { expect(subject.fields[:name].eq("DC Villan Size 6")).to include(product_one) }
+    it { expect(subject.fields[:name].eq("DC Villan Size 7")).to_not include(product_one) }
   end
 
+  context ".eq_lower" do
+    it { expect(subject.fields[:name].eq_lower("dc Villan Size 6")).to include(product_one) }
+  end
+
+  context ".matches" do
+    it { expect(subject.fields[:name].matches("dc")).to include(product_one, product_two) }
+  end
 end
