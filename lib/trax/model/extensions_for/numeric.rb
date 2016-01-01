1d395e7e272687f45610c1d96fb1c89b4ca6ac9c
Jason Ayre <jasonayre@gmail.com>
2016-01-01 14:44:05 -0700

    numeric attribute field extensions begin

--- lib/trax/model/extensions_for/numeric.rb
+++ lib/trax/model/extensions_for/numeric.rb
@@ -17,32 +17,27 @@ module Trax
 
           def eq(*_scope_values)
             _scope_values.flat_compact_uniq!
-            cast_type = type
-            model_class.where("(#{field_name})::#{cast_type} IN(?)", _scope_values)
+            model_class.where({field_name => _scope_values})
           end
 
           def gt(value)
-            cast_type = type
             operator = '>'
-            model_class.where("(#{field_name})::#{cast_type} #{operator} ?", value)
+            model_class.where("#{field_name} #{operator} ?", value)
           end
 
           def gte(value)
-            cast_type = type
             operator = '>='
-            model_class.where("(#{parent_definition.field_name} ->> '#{field_name}')::#{cast_type} #{operator} ?", value)
+            model_class.where("#{field_name} #{operator} ?", value)
           end
 
           def lt(value)
-            cast_type = type
             operator = '<'
-            model_class.where("(#{parent_definition.field_name} ->> '#{field_name}')::#{cast_type} #{operator} ?", value)
+            model_class.where("#{field_name} #{operator} ?", value)
           end
 
           def lte(value)
-            cast_type = type
             operator = '<='
-            model_class.where("(#{parent_definition.field_name} ->> '#{field_name}')::#{cast_type} #{operator} ?", value)
+            model_class.where("#{field_name} #{operator} ?", value)
           end
         end
       end
--- spec/support/models.rb
+++ spec/support/models.rb
@@ -35,6 +35,10 @@ class Product < ::ActiveRecord::Base
 
   define_attributes do
     string :name
+    # float :price
+
+    integer :in_stock_quantity
+    integer :out_of_stock_quantity
 
     enum :status, :default => :in_stock do
       define :in_stock,     1
