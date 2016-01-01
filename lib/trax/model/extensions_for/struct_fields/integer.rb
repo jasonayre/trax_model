93ef9b816a86b1793e26a731daff06aad982940d
Jason Ayre <jasonayre@gmail.com>
2016-01-01 12:28:50 -0700

    add integer struct field extensions

---
+++ lib/trax/model/extensions_for/struct_fields/integer.rb
@@ -0,0 +1,12 @@
+module Trax
+  module Model
+    module ExtensionsFor
+      module StructFields
+        module Integer
+          extend ::ActiveSupport::Concern
+          include ::Trax::Model::ExtensionsFor::StructFields::Numeric
+        end
+      end
+    end
+  end
+end
--- lib/trax/model/extensions_for/struct_fields/time.rb
+++ lib/trax/model/extensions_for/struct_fields/time.rb
@@ -22,6 +22,30 @@ module Trax
             def between(first_time, second_time)
               after(first_time).merge(before(second_time))
             end
+
+            def gt(value)
+              after(value)
+            end
+
+            def gte(value)
+              cast_type = 'timestamp'
+              operator = '>='
+              model_class.where("(#{parent_definition.field_name} ->> '#{field_name}')::#{cast_type} #{operator} ?", value)
+            end
+
+            def in_range(first_time, second_time)
+              gte(first_time).merge(lte(second_time))
+            end
+
+            def lt(value)
+              before(value)
+            end
+
+            def lte(value)
+              cast_type = 'timestamp'
+              operator = '<='
+              model_class.where("(#{parent_definition.field_name} ->> '#{field_name}')::#{cast_type} #{operator} ?", value)
+            end
           end
         end
       end
