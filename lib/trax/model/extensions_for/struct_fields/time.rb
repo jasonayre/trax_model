9777ba81fb8504fe1df6721a05f1956992cc3c7d
Jason Ayre <jasonayre@gmail.com>
2016-01-01 10:25:41 -0700

    struct fields time extensions

--- lib/trax/model/extensions_for/struct_fields/time.rb
+++ lib/trax/model/extensions_for/struct_fields/time.rb
@@ -7,33 +7,7 @@ module Trax
 
           include ::Trax::Model::ExtensionsFor::Base
 
-          # return unless has_active_record_ancestry?(parent_definition)
-
-          # included do
-          #   # model_class = model_class_for_property(property_klass)
-          #   attribute_klass = parent_definition
-          #   # field_name = property_klass.parent_definition.name.demodulize.underscore
-          #   attribute_name = property_klass.name.demodulize.underscore
-          #   cast_type = 'timestamp'
-          #
-          #   { :gt => '>', :lt => '<'}.each_pair do |k, operator|
-          #     scope_prefix = as ? as : :"by_#{field_name}_#{attribute_name}"
-          #     scope_name = "#{scope_prefix}_#{k}"
-          #     # scope_alias = "#{scope_prefix}_#{{:gt => 'after', :lt => 'before' }[k]}"
-          #
-          #     model_class.scope(scope_name, lambda{ |*_scope_values|
-          #       _scope_values.flat_compact_uniq!
-          #       model_class.where("(#{field_name} ->> '#{attribute_name}')::#{cast_type} #{operator} ?", _scope_values)
-          #     })
-          #     model_class.singleton_class.__send__("alias_method", scope_alias.to_sym, scope_name)
-          #   end
-          # end
-
           module ClassMethods
-            # def gt
-            #   model_class.all
-            # end
-
             def after(*_scope_values)
               _scope_values.flat_compact_uniq!
               cast_type = 'timestamp'
@@ -46,6 +20,10 @@ module Trax
               cast_type = 'timestamp'
               operator = '<'
               model_class.where("(#{parent_definition.field_name} ->> '#{field_name}')::#{cast_type} #{operator} ?", _scope_values)
+            end
+
+            def between(first_time, second_time)
+              after(first_time).merge(before(second_time))
             end
           end
         end
