d802cdf98aab2d61b3997fb7a0f9c7708d59ac7a
Jason Ayre <jasonayre@gmail.com>
2016-03-21 16:56:10 -0600

    and actually fix, remove inclusion from base

--- lib/trax/model/base.rb
+++ lib/trax/model/base.rb
@@ -3,10 +3,6 @@ module Trax
     module Base
       extend ::ActiveSupport::Concern
 
-      included do
-        self::ActiveRecord_Relation.include(::Trax::Model::RelationMethods)
-      end
-
       def dig(*args)
         try_chain(*args)
       end
