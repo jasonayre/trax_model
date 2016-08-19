1a12ef0239368155a32d5a011989513d492788d1
Jason Ayre <jayre@peer60.com>
2016-08-19 10:55:59 -0600

    append reserved subdomains dont reassign

--- lib/trax/validators/subdomain_validator.rb
+++ lib/trax/validators/subdomain_validator.rb
@@ -2,7 +2,7 @@ class SubdomainValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     return unless value.present?
     reserved_names = %w(www ftp mail pop smtp admin ssl sftp)
-    reserved_names = options[:reserved] if options[:reserved]
+    reserved_names += options[:reserved] if options[:reserved]
 
     if reserved_names.include?(value)
       object.errors[attribute] << 'cannot be a reserved name'
