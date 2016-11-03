8645c010b41bf553a500f3075a1f77d50e3baf46
Jason Ayre <jayre@peer60.com>
2016-11-03 12:46:17 -0600

    extend_cache_store reverse extends the cache store instance

--- lib/trax/model/cache_store_extensions.rb
+++ lib/trax/model/cache_store_extensions.rb
@@ -1,6 +1,11 @@
 module Trax
   module Model
     module CacheStoreExtensions
+      def self.extend_cache_store(cache_store)
+        cache_store.singleton_class.include(::Trax::Model::CacheStoreExtensions)
+        cache_store
+      end
+
       def fetch(*args, **options)
         if(args.first.is_a?(::Trax::Model::CacheKey))
           cache_key = args.first
