4c24fba9ce9a69a47149cadcfd9924dbf1667d0b
Jason Ayre <jayre@peer60.com>
2016-11-03 12:12:01 -0600

    cleanup spec

--- spec/trax/model/cache_key_spec.rb
+++ spec/trax/model/cache_key_spec.rb
@@ -1,27 +1,24 @@
 require 'spec_helper'
 
 describe ::Trax::Model::CacheKey do
-  subject{ described_class }
+  let(:fake_id) { 1 }
+  let(:expires) { 2.hours }
+  let(:cache_options) { {:expires_in => expires } }
+  let(:search_params) { {:id => fake_id} }
+  let(:cache_key_params) { cache_options.merge(search_params) }
+  subject{ described_class.new("posts", **cache_key_params) }
 
-  describe ".new" do
-    let(:fake_id) { 1 }
-    let(:expires) { 2.hours }
-    let(:cache_options) { {:expires_in => expires } }
-    let(:search_params) { {:id => fake_id} }
-    let(:cache_key_params) { cache_options.merge(search_params) }
-    subject { ::Trax::Model::CacheKey.new("posts", **cache_key_params) }
-    it { expect(subject.first).to eq "posts" }
-    it { expect(subject.options).to eq cache_options }
-    it { expect(subject.to_s).to eq "posts/id/1"}
+  it { expect(subject.first).to eq "posts" }
+  it { expect(subject.options).to eq cache_options }
+  it { expect(subject.to_s).to eq "posts/id/1" }
 
-    context "order of params" do
-      let(:search_params) { {:id => fake_id, :a => "b"} }
+  context "order of params" do
+    let(:search_params) { {:id => fake_id, :a => "b"} }
+    it { expect(subject.to_s).to eq "posts/a/b/id/1" }
+
+    context "does not matter" do
+      let(:search_params) { {:a => "b", :id => fake_id} }
       it { expect(subject.to_s).to eq "posts/a/b/id/1" }
-
-      context "does not matter" do
-        let(:search_params) { {:a => "b", :id => fake_id} }
-        it { expect(subject.to_s).to eq "posts/a/b/id/1" }
-      end
     end
   end
 end
