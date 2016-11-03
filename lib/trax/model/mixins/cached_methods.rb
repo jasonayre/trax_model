fc012e6b6c21e9c105ff65db82e00775fce945ea
Jason Ayre <jayre@peer60.com>
2016-11-03 16:13:30 -0600

    cached methods mixin

---
+++ lib/trax/model/mixins/cached_methods.rb
@@ -0,0 +1,55 @@
+module Trax
+  module Model
+    module Mixins
+      module CachedMethods
+        extend ::Trax::Model::Mixin
+
+        module ClassMethods
+          def cached_class_method(method_name, **config_options)
+            define_singleton_method("cached_#{method_name}") do |*args, **options|
+              method_cache_key = ::Trax::Model::CacheKey.for_class_method(self, method_name, *args, **config_options.merge(options))
+              has_args = method(method_name).arity != 0
+              has_options = method(method_name).parameters.any?
+
+              ::Trax::Model.cache.fetch(method_cache_key) do
+                result = if !has_args && !has_options
+                  __send__(method_name)
+                elsif has_args && has_options
+                  __send__(method_name, *args, **options)
+                elsif has_args
+                  __send__(method_name, *args)
+                elsif has_options
+                  __send__(method_name, **options)
+                end
+
+                result
+              end
+            end
+          end
+
+          def cached_instance_method(method_name, **config_options)
+            define_method("cached_#{method_name}") do |*args, **options|
+              method_cache_key = ::Trax::Model::CacheKey.for_instance_method(self.class, method_name, *args, **config_options.merge(options))
+              has_args = method(method_name).arity != 0
+              has_options = method(method_name).parameters.any?
+
+              ::Trax::Model.cache.fetch(method_cache_key) do
+                result = if !has_args && !has_options
+                  __send__(method_name)
+                elsif has_args && has_options
+                  __send__(method_name, *args, **options)
+                elsif has_args
+                  __send__(method_name, *args)
+                elsif has_options
+                  __send__(method_name, **options)
+                end
+
+                result
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
