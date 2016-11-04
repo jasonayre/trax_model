2f51171770cb3e4d1223314e0b72ae141148ff86
Jason Ayre <jayre@peer60.com>
2016-11-04 08:27:42 -0600

    fix cached instance method, use id as part of cache key

--- lib/trax/model/mixins/cached_methods.rb
+++ lib/trax/model/mixins/cached_methods.rb
@@ -8,47 +8,19 @@ module Trax
           def cached_class_method(method_name, **config_options)
             define_singleton_method("cached_#{method_name}") do |*args, **options|
               method_cache_key = ::Trax::Model::CacheKey.for_class_method(self, method_name, *args, **config_options.merge(options))
-              accepts_args = method(method_name).arity != 0
-              has_options = !options.empty?
-
-              binding.pry
 
               ::Trax::Model.cache.fetch(method_cache_key) do
-                result = if !accepts_args
-                  __send__(method_name)
-                elsif accepts_args
-                  __send__(method_name, *args, **options)
-                elsif accepts_args && has_options
-                  __send__(method_name, *args, **options)
-                elsif accepts_args
-                  __send__(method_name, *args)
-                elsif has_options
-                  __send__(method_name, **options)
-                end
-
-                result
+                __smartsend__(method_name, *args, **options)
               end
             end
           end
 
           def cached_instance_method(method_name, **config_options)
             define_method("cached_#{method_name}") do |*args, **options|
-              method_cache_key = ::Trax::Model::CacheKey.for_instance_method(self.class, method_name, *args, **config_options.merge(options))
-              accepts_args = method(method_name).arity != 0
-              has_options = method(method_name).parameters.any?
+              method_cache_key = ::Trax::Model::CacheKey.for_instance_method(self.class, method_name, self.id, *args, **config_options.merge(options))
 
               ::Trax::Model.cache.fetch(method_cache_key) do
-                result = if !accepts_args && !has_options
-                  __send__(method_name)
-                elsif accepts_args && has_options
-                  __send__(method_name, *args, **options)
-                elsif accepts_args
-                  __send__(method_name, *args)
-                elsif has_options
-                  __send__(method_name, **options)
-                end
-
-                result
+                __smartsend__(method_name, *args, **options)
               end
             end
           end
