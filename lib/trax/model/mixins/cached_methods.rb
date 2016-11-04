fe75c41a5d0ce983f3dcee0155d04ca356b03ebe
Jason Ayre <jayre@peer60.com>
2016-11-04 09:45:02 -0600

    define cache clearer methods for each cached method definition

--- lib/trax/model/mixins/cached_methods.rb
+++ lib/trax/model/mixins/cached_methods.rb
@@ -7,6 +7,7 @@ module Trax
         module ClassMethods
           def cached_class_method(method_name, as:nil, **config_options)
             cached_method_name = as || "cached_#{method_name}"
+            cache_clearer_method_name = "clear_#{cached_method_name}"
 
             define_singleton_method(cached_method_name) do |*args, **options|
               method_cache_key = ::Trax::Model::CacheKey.for_class_method(self, method_name, *args, **config_options.merge(options))
@@ -15,10 +16,17 @@ module Trax
                 __smartsend__(method_name, *args, **options)
               end
             end
+
+            define_singleton_method(cache_clearer_method_name) do |*args, **options|
+              method_cache_key = ::Trax::Model::CacheKey.for_class_method(self, method_name, *args, **config_options.merge(options))
+
+              ::Trax::Model.cache.delete(method_cache_key)
+            end
           end
 
           def cached_instance_method(method_name, as:nil, **config_options)
             cached_method_name = as || "cached_#{method_name}"
+            cache_clearer_method_name = "clear_#{cached_method_name}"
 
             define_method(cached_method_name) do |*args, **options|
               method_cache_key = ::Trax::Model::CacheKey.for_instance_method(self.class, method_name, self.id, *args, **config_options.merge(options))
@@ -26,6 +34,12 @@ module Trax
               ::Trax::Model.cache.fetch(method_cache_key) do
                 __smartsend__(method_name, *args, **options)
               end
+            end
+
+            define_method(cache_clearer_method_name) do |*args, **options|
+              method_cache_key = ::Trax::Model::CacheKey.for_instance_method(self.class, method_name, self.id, *args, **config_options.merge(options))
+
+              ::Trax::Model.cache.delete(method_cache_key)
             end
           end
         end
