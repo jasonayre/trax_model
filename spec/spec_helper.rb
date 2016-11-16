63820282295b1de200cba2e9c2f65cef5edd0591
Jason Ayre <jasonayre@gmail.com>
2016-11-16 16:44:31 -0700

    fix spec setup/connections

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,12 +15,8 @@ end
 
 ENV["DB"] ||= "sqllite"
 ENV["DB"] = "postgres" if ENV["DB"] == "pg" || ENV["pg"] == "true"
-
-if ENV["DB"] == "postgres" && ENV["CI"] == "true"
-  ENV["DATABASE_URL"] = "localhost"
-else
-  ENV["DATABASE_URL"] = "localhost:6432"
-end
+ENV["DB_PORT"] = (ENV["DB"] == "postgres" && ENV["CI"] == "true") ? "5432" : "6432"
+ENV["DATABASE_URL"] = "postgres://postgres@localhost:#{ENV['DB_PORT']}/trax_model_tests"
 
 RSpec.configure do |config|
   config.filter_run :focus
@@ -28,7 +24,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
 
   config.before(:suite) do
-    db_config = ::YAML::load(::File.open("#{File.dirname(__FILE__)}/db/database.yml"))
+    db_config = ::YAML::load(::ERB.new(::File.read("#{File.dirname(__FILE__)}/db/database.yml")).result)
 
     ::ActiveRecord::Base.establish_connection(db_config[ENV["DB"]])
 
